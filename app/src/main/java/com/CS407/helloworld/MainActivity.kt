252a724d4444b939737c17197d7dd3af4da62981
lsimonen1 <123672618+lsimonen1@users.noreply.github.com>
2024-09-16 15:02:22 -0500

    second edit

--- app/src/main/java/com/CS407/helloworld/MainActivity.kt
+++ app/src/main/java/com/CS407/helloworld/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.CS407.helloworld
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    //Imageview
+    private lateinit var ingView:ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
